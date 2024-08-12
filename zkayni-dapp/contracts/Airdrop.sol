ef21bef4ad8bd9d57a52d14992630ea667708347
ChioGT <chio.gonzalezt@gmail.com>
2024-08-11 21:03:46 -0500

    Update contracts and test

--- zkayni-dapp/contracts/Airdrop.sol
+++ zkayni-dapp/contracts/Airdrop.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./BeneficiaryProof.sol";
+
+
 
 contract Airdrop is Ownable {
     IERC20 private token;
@@ -12,13 +15,16 @@ contract Airdrop is Ownable {
     uint256 private numBeneficiaries;
     uint256 public amountPerBeneficiary;
     bytes32[] private joinKeys;
+    uint randNonce = 0;
     
-    mapping(bytes32 => bool) public beneficiaries;
-    mapping(bytes32 => uint256) public balances;
+    mapping(address => bool) public beneficiaries;
+    mapping(address => uint256) public balances;
+    mapping(uint256 => uint256) public vouchers;
+    mapping(uint256 => bool) public isVoucherAvailable;
 
     event GroupCreated(address indexed manager, uint256 totalAmount, uint256 numBeneficiaries);
     event VoucherCreated(bytes32 indexed beneficiary, uint256 amount);
-    event Withdrawal(address indexed thirdParty, uint256 amount);
+    
 
     constructor(address _token) Ownable(msg.sender){
         token = IERC20(_token);
@@ -34,44 +40,43 @@ contract Airdrop is Ownable {
     }
 
     function generateBeneficiaryProof( address[] memory _beneficiaries) private {
-        beneficiaryProof  = new BeneficiaryProof(_beneficiaries);
-        bytes32[] memory hashes = beneficiaryProof.getLeafs();
-        for (uint256 i = 0; i < hashes.length; i++) {
-            joinGroup(hashes[i]);
-            balances[hashes[i]] = amountPerBeneficiary;
+        for (uint256 i = 0; i < _beneficiaries.length; i++) {
+            joinGroup(_beneficiaries[i]);
+            balances[_beneficiaries[i]] = amountPerBeneficiary;
         }
     }
 
-    function joinGroup(bytes32 _beneficiary) public {
+    function joinGroup(address _beneficiary) public {
         require(beneficiaries[_beneficiary] == false, "Already a beneficiary");
         beneficiaries[_beneficiary] = true;
         balances[_beneficiary] += amountPerBeneficiary;
     }
 
-    function createVoucher(bytes32 _beneficiary, uint256 _amount) public returns (bool) {
+    function registerVoucher(address _beneficiary, uint256 _voucher, uint256 _amount) external onlyOwner {
         require(beneficiaries[_beneficiary] == true, "Not a beneficiary");
         require(balances[_beneficiary] >= _amount, "Insufficient balance");
         balances[_beneficiary] -= _amount;
-
-        // emit VoucherCreated(_beneficiary, _amount);
-        return true;
-    }
-
-    function getBeneficiaryHashes() external view onlyOwner returns (bytes32[] memory)  {
-        return beneficiaryProof.getLeafs();
+        vouchers[_voucher] = _amount;
+        isVoucherAvailable[_voucher] = true;
     }
 
      function getTotalAmount() external view onlyOwner returns (uint256)  {
         return totalAmount;
     }
 
-    function getBalance(bytes32 _beneficiary) external view onlyOwner returns (uint256)  {
+    function getBalance(address _beneficiary) external view onlyOwner returns (uint256)  {
         return balances[_beneficiary];
     }
 
-    function withdraw(address _thirdParty, uint256 _amount) public {
-        token.transfer(_thirdParty, _amount);
-        emit Withdrawal(_thirdParty, _amount);
+    function getVoucherAmount(uint256 _voucher) external view onlyOwner returns (uint256)  {
+        return vouchers[_voucher];
+    }
+
+    function withdraw(address _thirdParty, uint256 _voucher) public  {
+        require(isVoucherAvailable[_voucher] != false, "Invalid voucher");
+        require(vouchers[_voucher] != 0, "Invalid voucher amount");
+        token.transfer(_thirdParty, vouchers[_voucher]);
+        isVoucherAvailable[_voucher] = false;
     }
 
 }
--- zkayni-dapp/test/test.js
+++ zkayni-dapp/test/test.js
@@ -2,7 +2,8 @@ const { expect } = require("chai");
 
 describe("Airdrop contract", function () {
   it("Deployment creates a group", async function () {
-    const [owner, addr1, addr2, addr3, addr4, addr5, addr6,addr7, addr8, addr9, addr10] = await ethers.getSigners();
+
+    const [owner, addr1, addr2, addr3, addr4, addr5, addr6,addr7, addr8, addr9, addr10, thirdParty] = await ethers.getSigners();
 
     const Token = await ethers.getContractFactory("Token");
     const token = await ethers.deployContract("Token");
@@ -31,13 +32,63 @@ describe("Airdrop contract", function () {
     // console.log("Airdrop number of beneficiaries: ", airdrop.numBeneficiaries);
     // console.log("Airdrop amount per beneficiary: ", airdrop.amountPerBeneficiary);
 
-    console.log("Airdrop beneficiary keys: ", await airdrop.getBeneficiaryHashes());
-    
-    var hashes = await airdrop.getBeneficiaryHashes();
+    console.log("Test account: ", await beneficiaries[8].getAddress());
+    console.log("Checking balance equals to 100...");
+    console.log("Beneficiary balance: ", await airdrop.getBalance(beneficiaries[8]));
 
-    console.log("Test account: ", hashes[8]);
-    console.log("Airdrop balance for: ", await airdrop.getBalance(hashes[8]));
-    // expect(await airdrop.amountPerBeneficiary()).to.equal(100);
-    expect(await airdrop.createVoucher(hashes[8],100));
+    console.log("");
+    console.log("Creating 10 tokens voucher...");
+    voucher = Math.floor(Math.random() * 10000000000);
+    await airdrop.registerVoucher(beneficiaries[8], voucher ,10);
+    console.log("Checking balance equals to 90...");
+    console.log("Beneficiary balance : ", await airdrop.getBalance(beneficiaries[8]));
+
+    console.log("");
+    console.log("Creating 80 tokens voucher...");
+    voucher = Math.floor(Math.random() * 10000000000);
+    testVoucher = voucher;
+    await airdrop.registerVoucher(beneficiaries[8], voucher ,80);
+    console.log("Created voucher: ", voucher);
+    console.log("Checking balance equals to 10...");
+    console.log("Beneficiary balance : ", await airdrop.getBalance(beneficiaries[8]));
+
+    console.log("");
+    console.log("Creating 20 tokens voucher exceding the available balance...");
+    try {
+      voucher = Math.floor(Math.random() * 10000000000);
+      await airdrop.registerVoucher(beneficiaries[8], voucher ,20);
+    } catch (error) {
+      if (error.message.includes("Insufficient balance")){
+        console.log("Correctly refused by Insufficient balance");
+      }else{
+        console.error(error);
+      }
+    }
+    console.log("");
+    console.log("Checking balance remains to 10...");
+    console.log("Beneficiary balance:", await airdrop.getBalance(beneficiaries[8]));
+
+    console.log("");
+    console.log("Withdrwal 80 tokens voucher ", testVoucher);
+    console.log("Third party balance before withdrawal: ", await token.balanceOf(thirdParty));
+    console.log("Contract balance before withdrawal: ", await token.balanceOf(airdrop.getAddress()));
+    console.log("Test voucher amount: ", await airdrop.getVoucherAmount(testVoucher));
+    try {
+      console.log("Withdrawing .... ");
+      await airdrop.withdraw(thirdParty, testVoucher);
+    } catch (error) {
+      if (error.message.includes("Invalid voucher")){
+        console.log("Refused by Invalid voucher");
+      }else{
+        console.error(error);
+      }
+    }
+    console.log("Third party balance after withdrawal: ", await token.balanceOf(thirdParty));
+    console.log("Contract balance after withdrawal: ", await token.balanceOf(airdrop.getAddress()));
+
+    console.log("");
+    console.log("Checking balance remains to 10...");
+    expect(await airdrop.getBalance(beneficiaries[8])).equals(10);
+
   });
 });
